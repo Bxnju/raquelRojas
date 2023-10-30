7b4959b4aa1ea1d5b0e839139fc05bedad9b26e6
Bxnju <andres.benjumea@upb.edu.co>
2023-10-30 12:49:39 -0500

    feat: the button to play the audios was added and is functional

--- js/script.js
+++ js/script.js
@@ -10,3 +10,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const miAudio = document.getElementById("miAudio");
     miAudio.volume = 0.2;
 });
+
+const miAudio = document.getElementById("miAudio");
+const btnAudio = document.getElementById("btnAudio");
+let cont = 0;
+
+function playAndPause() {
+    
+    if(cont == 0){
+
+        miAudio.play();
+        cont++;
+
+    }else if(cont % 2 == 0){
+        btnAudio.textContent = "volume_up";
+        miAudio.play();
+        cont++;
+    }else{
+        btnAudio.textContent = "volume_off";
+        miAudio.pause();
+        cont++;
+    }
+
+}
